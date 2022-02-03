fed1b47b35aa87bcdf7bb9ed52f0e06246026b3d
LazarevaKate <lazareva.ekaterina2yandex.ru>
2022-02-03 17:20:03 +0300

    Задание выполнено

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -10,11 +10,11 @@ from django.views.generic import CreateView
 
 from django.urls import reverse_lazy
 
-from posts.models import Post
-
 from . import forms
 
 from .forms import CreationForm
+
+from ..posts.models import Post
 
 
 class SignUp(CreateView):
